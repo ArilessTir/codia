2d1f42bf257b4b6e7c9e14fc7ceedc207562b9a2
ArilessTir <ariless.tir@gmail.com>
2022-04-24 15:36:20 +0200

    change icons :lipstick:

--- src/components/sideNav/sideNav.jsx
+++ src/components/sideNav/sideNav.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import {
     RiChinaRailwayLine,
     RiBankFill,
-    RiBookmark2Fill,
+    RiFileCodeFill,
     RiBriefcaseFill,
     RiLogoutBoxLine,
+    RiQuestionFill,
+    RiBook2Fill,
 } from 'react-icons/ri';
+import { AiFillProject } from 'react-icons/ai';
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 import gsap from 'gsap';
 
@@ -16,29 +19,29 @@ const SideNav = () => {
         setOpen(!open);
         console.log(open);
         if (open === true) {
-            gsap.to('.sideBar', { width: '40px', paddingLeft: '0' });
+            gsap.to('.sideBar', { minWidth: '35px', paddingLeft: '0' });
         } else {
-            gsap.to('.sideBar', { width: '240px', paddingLeft: '8px' });
+            gsap.to('.sideBar', { minWidth: '200px', paddingLeft: '8px' });
         }
     };
 
     return (
         <div
-            className="sideBar h-[100vh] w-[240px] bg-[#282944]
-                    text-white fixed overflow-hidden pl-2 pt-20
+            className="sideBar h-[100vh] min-w-[200px] bg-[#282944]
+                    text-white fixed  pl-2 pt-20
                     flex flex-col justify-between"
         >
             {open ? (
                 <HiChevronDoubleLeft
                     size={30}
                     onClick={handleOpen}
-                    className="cursor-pointer"
+                    className="cursor-pointer hover:bg-white hover:text-[#282944] rounded-md"
                 />
             ) : (
                 <HiChevronDoubleRight
                     size={30}
                     onClick={handleOpen}
-                    className="cursor-pointer"
+                    className="cursor-pointer hover:bg-white hover:text-[#282944] rounded-md"
                 />
             )}
             <section className="space-y-2 mb-20">
@@ -53,15 +56,29 @@ const SideNav = () => {
                     className=" flex items-center cursor-pointer
                                 hover:bg-white hover:text-[#282944] rounded-md h-12"
                 >
-                    <RiBankFill className="fixed" size={30} />
+                    <RiFileCodeFill className="fixed" size={30} />
                     <div className="txt text-2xl ml-10">Exercices</div>
                 </div>
                 <div
                     className=" flex items-center cursor-pointer
                                 hover:bg-white hover:text-[#282944] rounded-md h-12"
                 >
-                    <RiBookmark2Fill className="fixed" size={30} />
+                    <RiBook2Fill className="fixed" size={30} />
                     <div className="txt text-2xl ml-10">Cours</div>
+                </div>
+                <div
+                    className=" flex items-center cursor-pointer
+                                hover:bg-white hover:text-[#282944] rounded-md h-12"
+                >
+                    <RiQuestionFill className="fixed" size={30} />
+                    <div className="txt text-2xl ml-10 ">Quizz</div>
+                </div>
+                <div
+                    className=" flex items-center cursor-pointer
+                                hover:bg-white hover:text-[#282944] rounded-md h-12"
+                >
+                    <AiFillProject className="fixed" size={30} />
+                    <div className="txt text-2xl ml-10 ">Projets</div>
                 </div>
                 <div
                     className=" flex items-center cursor-pointer
