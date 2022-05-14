f4b58139698315d34ec4a2ef6530fa615de0ab59
ArilessTir <ariless.tir@gmail.com>
2022-05-14 15:46:51 +0200

    init courses

---
+++ src/data/courses.js
@@ -0,0 +1,28 @@
+export const courses = [
+    {
+        Metadata: {
+            Title: 'Analyse Univarié',
+            Author: 'Ariless Tir',
+            Photo: '',
+        },
+        Data: [
+            {
+                Title: 'Les types de données',
+                Content: '',
+                Photo: '',
+            },
+            {
+                Title: 'Les données quantitatives',
+                Content:
+                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, ratione dolores. Consequatur voluptatem ipsam doloribus consequuntur nihil autem aliquid nam fugit aspernatur quasi nulla dolores, animi rem placeat dolorem sit!               Repudiandae, adipisci? Libero possimus dolorum non, aspernatur placeat beatae! Quod eligendi quo voluptas omnis dolor, cum labore nihil voluptatem perferendis dignissimos, unde minus accusamus tenetur doloribus. Molestiae tempore hic consequuntur.                Exercitationem minus soluta, ipsam quia ut alias. Tenetur consequatur a praesentium eligendi natus molestiae exercitationem. Reprehenderit natus debitis voluptates, voluptatum quam at nihil dolores obcaecati molestias perspiciatis vel vitae a.                Omnis, eaque voluptate similique corrupti dolorem temporibus eveniet, ea velit quia quis accusamus? Expedita ipsum minima placeat tenetur nostrum, reprehenderit ea? Culpa in molestias voluptas blanditiis reiciendis, officia mollitia consectetur?     Similique deleniti modi repellat assumenda rem. Facilis ducimus fugiat consequatur deleniti quam quasi adipisci alias natus totam aliquam. Quia maiores non distinctio quae atque id ipsam aliquam nihil deserunt exercitationem.',
+                Photo: '',
+            },
+            {
+                Title: 'Les données Qualitatives',
+                Content:
+                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, ratione dolores. Consequatur voluptatem ipsam doloribus consequuntur nihil autem aliquid nam fugit aspernatur quasi nulla dolores, animi rem placeat dolorem sit!               Repudiandae, adipisci? Libero possimus dolorum non, aspernatur placeat beatae! Quod eligendi quo voluptas omnis dolor, cum labore nihil voluptatem perferendis dignissimos, unde minus accusamus tenetur doloribus. Molestiae tempore hic consequuntur.                Exercitationem minus soluta, ipsam quia ut alias. Tenetur consequatur a praesentium eligendi natus molestiae exercitationem. Reprehenderit natus debitis voluptates, voluptatum quam at nihil dolores obcaecati molestias perspiciatis vel vitae a.                Omnis, eaque voluptate similique corrupti dolorem temporibus eveniet, ea velit quia quis accusamus? Expedita ipsum minima placeat tenetur nostrum, reprehenderit ea? Culpa in molestias voluptas blanditiis reiciendis, officia mollitia consectetur?     Similique deleniti modi repellat assumenda rem. Facilis ducimus fugiat consequatur deleniti quam quasi adipisci alias natus totam aliquam. Quia maiores non distinctio quae atque id ipsam aliquam nihil deserunt exercitationem.',
+                Photo: '',
+            },
+        ],
+    },
+];
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { courses } from '../data/courses';
 const Courses = () => {
+    console.log(courses[0]['Metadata']);
     return (
         <section className="pt-20 md:pl-60 px-5 sm:px-0">
-            <h1>Cours</h1>
+            <div>Cours</div>
         </section>
     );
 };
